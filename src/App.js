52418356dcc7f52089ee60b0d5dd8acc4cedabb4
wiusxt <ashatovanas65@gmail.com>
2022-06-08 14:13:02 +0600

    added h1 and start working with color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const color = {
+  red: Math.floor(Math.random() * 255) ,
+  green: Math.floor(Math.random() * 255) ,
+  blue: Math.floor(Math.random() * 255) ,
+};
+
+const styles = {
+  // color: `rgb(255, 255, 255)`
+  color: `rgb(${color.red}, ${color.green}, ${color.blue})` ,
+}
+
+return (
+  <div className="App">
+<h1>Hello World</h1>
+  </div>
+)
 }
 
 export default App;
