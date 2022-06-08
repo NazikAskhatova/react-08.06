9316d8e83448646eb8568a1271b45959337d59ac
wiusxt <ashatovanas65@gmail.com>
2022-06-08 14:19:21 +0600

    deleted some files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 
 function App() {
+const [number, setNumber] = useState(0);
+
+// alert("WElcome!");
+
+// localStorage
+
+// document.title = "Hello every1!"
+
+// setInterval setTimeout
+
+// Side Effect
 const color = {
   red: Math.floor(Math.random() * 255) ,
   green: Math.floor(Math.random() * 255) ,
   blue: Math.floor(Math.random() * 255) ,
 };
-
 const styles = {
   // color: `rgb(255, 255, 255)`
   color: `rgb(${color.red}, ${color.green}, ${color.blue})` ,
